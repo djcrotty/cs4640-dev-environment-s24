764093985a5cb77bfe94e15bf494143e55bc0486
djcrotty <djchappyman@verizon.net>
2024-05-02 21:55:17 -0400

    fixed dist

--- web/www/hw8/angularWords/dist/angular-words/server/main.server.mjs
+++ web/www/hw8/angularWords/dist/angular-words/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RARH2X4R.mjs";import"./chunk-YECH6D6F.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-V4HO562F.mjs";import"./chunk-YECH6D6F.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
